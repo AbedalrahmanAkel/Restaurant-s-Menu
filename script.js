4be40f66f1812271bc0d062497d4fcd5b9edb560
Abed Al Rahman Akel <abedalrahmanakl@gmail.com>
2023-12-20 12:07:02 +0200

    Added arrow on each section+js for it

--- script.js
+++ script.js
@@ -3,49 +3,70 @@ function loadAndClassifyProducts() {
     .then(response => response.text())
     .then(csv => {
       const data = csvToJson(csv);
-          data.forEach(product => {
-            if(product.Category.toUpperCase() == "JUICE"){
-            addProductToContainer(product,"Juice__sectionItems");
-            }
-            else if(product.Category.toUpperCase()=="COCKTAIL"){
-              addProductToContainer(product,"Cocktail__sectionItems");
-            }
-          });
-      })
-      .catch(error => console.error('Error:', error));
+      data.forEach(product => {
+        if (product.Category.toUpperCase() == "JUICE") {
+          addProductToContainer(product, "Juice__sectionItems");
+        }
+        else if (product.Category.toUpperCase() == "COCKTAIL") {
+          addProductToContainer(product, "Cocktail__sectionItems");
+        }
+      });
+    })
+    .catch(error => console.error('Error:', error));
 }
-   
-   function csvToJson(csv) {
-      const lines = csv.split(/\r?\n/);
-      const result = [];
-      const headers = lines[0].split(',');
-    
-      for (let i = 1; i < lines.length; i++) {
-          if (!lines[i]) continue;
-          const obj = {};
-          const currentline = lines[i].split(',');
-    
-          for (let j = 0; j < headers.length; j++) {
-              obj[headers[j]] = currentline[j];
-          }
-          result.push(obj);
-      }
-      return result;
+
+function csvToJson(csv) {
+  const lines = csv.split(/\r?\n/);
+  const result = [];
+  const headers = lines[0].split(',');
+
+  for (let i = 1; i < lines.length; i++) {
+    if (!lines[i]) continue;
+    const obj = {};
+    const currentline = lines[i].split(',');
+
+    for (let j = 0; j < headers.length; j++) {
+      obj[headers[j]] = currentline[j];
     }
-    function addProductToContainer(product,category) {
-      const templateContainer = document.querySelector('.item').cloneNode(true);
-      templateContainer.style.display = '';
-      const productNameElement = templateContainer.querySelector('.productName');
-          productNameElement.textContent = product['Product Name'];
-      const productPriceTag = templateContainer.querySelector('.priceTag');
-            productPriceTag.textContent = product["Price"];
-      const productDesc = templateContainer.querySelector('.productDesc')
-            productDesc.textContent = product["Desc"]
-      const imgElement = templateContainer.querySelector('.item__image');
-      if (imgElement && product['Image Link']) {
-          imgElement.setAttribute('src', product['Image Link']);
-      }
-      const parentContainer = document.querySelector(`.${category}`);
-      parentContainer.appendChild(templateContainer);
-    }
-    loadAndClassifyProducts();
+    result.push(obj);
+  }
+  return result;
+}
+function addProductToContainer(product, category) {
+  const templateContainer = document.querySelector('.item').cloneNode(true);
+  templateContainer.style.display = '';
+  const productNameElement = templateContainer.querySelector('.productName');
+  productNameElement.textContent = product['Product Name'];
+  const productPriceTag = templateContainer.querySelector('.priceTag');
+  productPriceTag.textContent = product["Price"];
+  const productDesc = templateContainer.querySelector('.productDesc')
+  productDesc.textContent = product["Desc"]
+  const imgElement = templateContainer.querySelector('.item__image');
+  if (imgElement && product['Image Link']) {
+    imgElement.setAttribute('src', product['Image Link']);
+  }
+  const parentContainer = document.querySelector(`.${category}`);
+  parentContainer.appendChild(templateContainer);
+}
+loadAndClassifyProducts();
+
+const sectionArrows = document.querySelectorAll('.sectionArrow');
+
+
+sectionArrows.forEach((arrow) => {
+  arrow.addEventListener('click', () => {
+    const itemsSection = arrow.parentElement.parentElement;
+    const itemsContainer = itemsSection.querySelector('.sectionItems');
+    const arrowContainer = itemsSection.querySelector('.arrowContainer');
+    itemsContainer.classList.toggle('hidden');
+    itemsSection.classList.toggle('collapse');
+    arrowContainer.classList.toggle('rotateArrow');
+    console.log(arrow)
+  })
+})
+
+
+
+
+
+
